e27187228eb5d7e992dcde0ef52bb57d3e8a1a01
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-09-04 23:26:03 -0300

    JChess updated

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,19 +1,35 @@
-package jchess;
+package com.chess.engine.board;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.chess.engine.pieces.Piece;
 
 public abstract class Tile {
 	
-	int tileCoordinate;
+	protected final int tileCoordinate;
+	private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
 	
 	Tile (int tileCoordinate) {
 		this.tileCoordinate = tileCoordinate;
 	}
 	
+	private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+		final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+		
+		for(int i = 0; i < 64; i++) {
+			emptyTileMap.put(i, new EmptyTile(i));
+		}
+		
+		return ImmutableMap.copyOf(emptyTileMap);
+	}
+
 	public abstract boolean isTileOccupied();
 	public abstract Piece getPiece();
 	
 	public static final class EmptyTile extends Tile {
 		
-		EmptyTile(int tileCoordinate) {
+		EmptyTile(final int tileCoordinate) {
 			super(tileCoordinate);
 		}
 		
@@ -30,7 +46,7 @@ public abstract class Tile {
 	
 	public static final class OccupiedTile extends Tile {
 		
-		Piece pieceOnTile;
+		private final Piece pieceOnTile;
 		
 		OccupiedTile (int tileCoordinate, Piece pieceOnTile) {
 			super(tileCoordinate);
